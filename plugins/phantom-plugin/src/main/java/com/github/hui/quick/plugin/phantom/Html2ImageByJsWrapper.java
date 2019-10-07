d2f3fa4cb242d0bc13349d05b2eb34bf5ae0a47d
quyixiao <2621048238@qq.com>
2019-10-07 16:24:46 +0800

    提交修改

--- plugins/phantom-plugin/src/main/java/com/github/hui/quick/plugin/phantom/Html2ImageByJsWrapper.java
+++ plugins/phantom-plugin/src/main/java/com/github/hui/quick/plugin/phantom/Html2ImageByJsWrapper.java
@@ -1,6 +1,10 @@
 package com.github.hui.quick.plugin.phantom;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -9,6 +13,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 通过phantomjs 来实现html转图片的功能，需要先安装phantomjs
@@ -18,13 +23,20 @@ import java.io.IOException;
  * <p>
  * Created by yihui on 2017/12/1.
  */
+
 public class Html2ImageByJsWrapper {
 
     private static PhantomJSDriver webDriver;
 
+
+
+    private static ChromeDriver chromeDriver;
+
     static {
         try {
             webDriver = getPhantomJs();
+            //chromeDriver = getChromeDriver();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -32,8 +44,28 @@ public class Html2ImageByJsWrapper {
 
     private static String getPhantomJsPath() {
         String path = System.getProperty("phantomjs.binary.path");
+
+
+        System.out.println("===========path==========="+path);
         if (path == null || "".endsWith(path)) {
-            return "/usr/local/bin/phantomjs";
+            return "/usr/local/bin/phantomjs/";
+        } else {
+            return path;
+        }
+    }
+
+
+    private static String getChromePath() {
+        String path = System.getProperty("phantomjs.binary.path");
+
+
+        // 如果不设置将搜索环境变量
+        System.setProperty("webdriver.chrome.driver", "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
+
+
+        System.out.println("===========path==========="+path);
+        if (path == null || "".endsWith(path)) {
+            return "/usr/local/bin/phantomjs/";
         } else {
             return path;
         }
@@ -58,8 +90,72 @@ public class Html2ImageByJsWrapper {
     }
 
 
+    private static ChromeDriver getChromeDriver() {
+        //设置必要参数
+        DesiredCapabilities dcaps = new DesiredCapabilities();
+        //ssl证书支持
+        dcaps.setCapability("acceptSslCerts", true);
+        //截屏支持
+        dcaps.setCapability("takesScreenshot", true);
+        //css搜索支持
+        dcaps.setCapability("cssSelectorsEnabled", true);
+        //js支持
+        dcaps.setJavascriptEnabled(true);
+        //驱动支持（第二参数表明的是你的phantomjs引擎所在的路径，which/whereis phantomjs可以查看）
+        // fixme 这里写了执行， 可以考虑判断系统是否有安装，并获取对应的路径 or 开放出来指定路径
+        dcaps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, getChromePath());
+        //创建无界面浏览器对象
+        return new ChromeDriver(dcaps);
+    }
+
+
     public static BufferedImage renderHtml2Image(String url) throws IOException {
         if (webDriver != null) {
+            //打开网址过后，设置隐性等待,等待10秒
+            webDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+            //打开网址后，窗口最大化
+            webDriver.manage().window().maximize();
+            float width = webDriver.manage().window().getSize().width;
+            float height = webDriver.manage().window().getSize().height;
+            System.out.println("pre with="+width + ",height="+height);
+
+            Dimension dimension = new Dimension(1000,800);
+            webDriver.manage().window().setSize(dimension);
+
+            width = webDriver.manage().window().getSize().width;
+
+            height = webDriver.manage().window().getSize().height;
+            System.out.println("set propety with="+width + ",height="+height);
+
+
+
+
+            webDriver.get(url);
+
+
+            File file = webDriver.getScreenshotAs(OutputType.FILE);
+
+
+
+
+
+            return ImageIO.read(file);
+        } else {
+            return null;
+        }
+    }
+
+
+
+    public static BufferedImage renderHtml2Image1(String url,int widthNew,int heightNew) throws IOException {
+        if (webDriver != null) {
+            //打开网址过后，设置隐性等待,等待10秒
+            webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            //打开网址后，窗口最大化
+            webDriver.manage().window().maximize();
+            int width = webDriver.manage().window().getSize().width;
+            int height = webDriver.manage().window().getSize().height;
+            System.out.println("pre with="+width + ",height="+height);
             webDriver.get(url);
             File file = webDriver.getScreenshotAs(OutputType.FILE);
             return ImageIO.read(file);
@@ -67,5 +163,16 @@ public class Html2ImageByJsWrapper {
             return null;
         }
     }
+
+
+
+
+    public static BufferedImage renderHtml2ImageByChromeDriver(String url,int widthNew,int heightNew) throws IOException {
+
+
+
+        return null;
+    }
+
 
 }
--- plugins/phantom-plugin/src/test/java/com/github/hui/quick/plugin/test/Html2ImgTest.java
+++ plugins/phantom-plugin/src/test/java/com/github/hui/quick/plugin/test/Html2ImgTest.java
@@ -1,12 +1,23 @@
 package com.github.hui.quick.plugin.test;
 
 import com.github.hui.quick.plugin.phantom.Html2ImageByJsWrapper;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.junit.Test;
 
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by yihui on 2018/3/26.
@@ -17,16 +28,154 @@ public class Html2ImgTest {
     public void testRender() throws IOException {
         try {
             BufferedImage img = null;
-            for (int i = 0; i < 20; ++i) {
-                String url = "https://my.oschina.net/u/566591/blog/1580020";
-                long start = System.currentTimeMillis();
-                img = Html2ImageByJsWrapper.renderHtml2Image(url);
-                long end = System.currentTimeMillis();
-                System.out.println("cost:  " + (end - start));
-            }
+            String url = "https://seleniumhq.github.io/selenium/docs/api/java/org/openqa/selenium/WebDriver.Window.html";
+            long start = System.currentTimeMillis();
+            int width = 3840;
+            int heght = 2560;
+            img = Html2ImageByJsWrapper.renderHtml2Image1(url,width,heght);
+            long end = System.currentTimeMillis();
+            System.out.println("cost:  " + (end - start));
+            //haha(img);
+
+            writePNGImage(img,"/Users/quyixiao/Desktop/aaabb/"+width+"*"+heght+".png");
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
+    @Test
+    public void testGoogleRender() throws IOException {
+        WebDriver driver =null;
+        BufferedImage img = null;
+        try {
+
+            String url = "https://www.echartsjs.com/examples/zh/editor.html?c=line-aqi";
+            long start = System.currentTimeMillis();
+            int width = 2440;
+            int heght = 1880;
+
+
+
+            //打开网址过后，设置隐性等待,等待10秒
+            // chromeDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            //打开网址后，窗口最大化
+            //chromeDriver.manage().window().maximize();
+
+            System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
+            driver = new ChromeDriver();
+            System.out.println("11111111111111111="+(System.currentTimeMillis()-start));
+           // driver.manage().window().setSize(new Dimension(width,heght));
+            driver.manage().window().fullscreen();
+            System.out.println("22222222222222222="+(System.currentTimeMillis()-start));
+            driver.get(url);
+            System.out.println("3333333333333333="+(System.currentTimeMillis()-start));
+            Thread.sleep(3000);
+           // driver.manage().window().setSize(new Dimension(width,heght));
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            System.out.println("44444444444444444="+(System.currentTimeMillis()-start));
+
+
+
+            img =  ImageIO.read(scrFile);
+
+
+
+            long end = System.currentTimeMillis();
+            System.out.println("cost:  " + (end - start));
+            //haha(img);
+
+            writePNGImage(img,"/Users/quyixiao/Desktop/aaabb/"+width+"*"+heght+".png");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+           driver.quit();
+        }
+    }
+
+
+    public static void haha(BufferedImage bf){
+
+        try {
+
+
+
+            File file = new File("/Users/quyixiao/Desktop/aaabb/aa.jpeg");
+            mkDir(file);
+            ImageIO.write(bf, "jpeg", file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 递归创建文件夹
+     *
+     * @param file 由目录创建的file对象
+     * @throws FileNotFoundException
+     */
+    public static void mkDir(File file) throws FileNotFoundException {
+        if (file.getParentFile() == null) {
+            file = file.getAbsoluteFile();
+        }
+
+        boolean ans;
+        if (file.getParentFile().exists()) {
+            modifyFileAuth(file);
+            if (!file.exists() && !file.mkdir()) {
+                throw new FileNotFoundException();
+            }
+        } else {
+            mkDir(file.getParentFile());
+            modifyFileAuth(file);
+            if (!file.exists() && !file.mkdir()) {
+                throw new FileNotFoundException();
+            }
+        }
+    }
+
+    /**
+     * 修改文件权限，设置为可读写
+     *
+     * @param file
+     */
+    private static void modifyFileAuth(File file) {
+        boolean ans = file.setExecutable(true, false);
+        ans = file.setReadable(true, false) && ans;
+        ans = file.setWritable(true, false) && ans;
+
+    }
+
+
+
+
+    public static boolean writeImage(RenderedImage im, String formatName, String fileName) {
+        boolean result = false;
+        try {
+            result = ImageIO.write(im,formatName,new File(fileName));
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return result;
+    }
+
+
+
+    public static boolean writeJPEGImage(RenderedImage im, String fileName) {
+        return writeImage(im,"JPEG",fileName);
+    }
+    public static boolean writeGIFImage(RenderedImage im, String fileName) {
+        return writeImage(im,"GIF",fileName);
+    }
+    public static boolean writePNGImage(RenderedImage im, String fileName) {
+        return writeImage(im,"PNG",fileName);
+    }
+    public static boolean writeBMPImage(RenderedImage im, String fileName) {
+        return writeImage(im,"BMP",fileName);
     }
 
 }
